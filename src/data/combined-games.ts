eae8c18e07142124cd7f13fb04b79e2200d2ffc2
Kangyann <kangyann727@gmail.com>
2025-06-09 11:58:07 +0700

    feat: new data for one use

---
+++ src/data/combined-games.ts
@@ -0,0 +1,7 @@
+import mlbbjson from "@/data/games/mobile-legends.json"
+import ffjson from "@/data/games/free-fire.json"
+
+export const CombinedGames: any = {
+    "mobile-legends": mlbbjson.data,
+    "free-fire": ffjson.data
+}
