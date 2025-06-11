7d4e3eaa047cb3924d4a7f7324f21c1b2681ada9
Kangyann <kangyann727@gmail.com>
2025-06-11 12:17:09 +0700

    feat: add new lib (isJson validation)

---
+++ src/lib/isJson.ts
@@ -0,0 +1,22 @@
+export async function isJson(request: Request): Promise<Record<string, string | number | boolean>> {
+    try {
+        const data: Record<string, any> = await request.json()
+        if (Object.keys(data).length == 0) {
+            return {
+                message: "Data is empty.",
+                status: 400,
+                isValid: false
+            }
+        }
+        return {
+            isValid: true,
+            ...data
+        };
+    } catch (error) {
+        return {
+            message: "Invalid JSON.",
+            status: 400,
+            isValid: false
+        }
+    }
+}
