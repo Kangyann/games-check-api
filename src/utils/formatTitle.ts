9dc6ce688851cef31dfab341b3c41edf1ca0c5ed
Kangyann <kangyann727@gmail.com>
2025-06-12 13:11:32 +0700

    chore: add docs at formatTitle

--- src/utils/formatTitle.ts
+++ src/utils/formatTitle.ts
@@ -1,6 +1,11 @@
+/**
+ * @function formatTitle
+ * @param {text: string}
+ * @returns {string}
+ */
 export function formatTitle({ text }: { text: string }): string {
     return text
-        .split('-') // pisah berdasarkan "-"
-        .map(word => word.charAt(0).toUpperCase() + word.slice(1)) // kapitalisasi
-        .join(' ') // gabung dengan spasi
+        .split('-')
+        .map(word => word.charAt(0).toUpperCase() + word.slice(1))
+        .join(' ')
 }
